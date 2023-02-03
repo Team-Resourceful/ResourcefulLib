bd402c99c43ecddbc12bdc052e10336c7c549842
James Moore <th3darkrose@gmail.com>
2023-02-03 00:45:40 -0500

    updated cacheable functions to allow map passthrough and added static methods for creating concurrent cacheable functions

--- common/src/main/java/com/teamresourceful/resourcefullib/common/caches/CacheableFunction.java
+++ common/src/main/java/com/teamresourceful/resourcefullib/common/caches/CacheableFunction.java
@@ -2,15 +2,21 @@ package com.teamresourceful.resourcefullib.common.caches;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Function;
 
 public class CacheableFunction<T, R> implements Function<T, R>, CachingFunction {
 
-    private final Map<T, R> cache = new HashMap<>();
+    private final Map<T, R> cache;
     private final Function<T, R> function;
 
-    public CacheableFunction(Function<T, R> function) {
+    public CacheableFunction(Function<T, R> function, Map<T, R> cache) {
+        this.cache = cache;
         this.function = function;
+    }
+
+    public CacheableFunction(Function<T, R> function) {
+        this(function, new HashMap<>());
     }
 
     @Override
@@ -20,6 +26,10 @@ public class CacheableFunction<T, R> implements Function<T, R>, CachingFunction 
 
     public void clear() {
         this.cache.clear();
+    }
+
+    public static <T, R> CacheableFunction<T, R> concurrent(Function<T, R> function) {
+        return new CacheableFunction<>(function, new ConcurrentHashMap<>());
     }
 
 }
