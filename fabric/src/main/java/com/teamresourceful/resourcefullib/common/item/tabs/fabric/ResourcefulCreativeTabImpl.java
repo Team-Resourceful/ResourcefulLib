163051572d0e8faa9aabd0663348987aaf7b7371
ThatGravyBoat <thatgravyboat@gmail.com>
2023-05-26 20:09:29 -0230

    Fix fabric 1.20.x

--- fabric/src/main/java/com/teamresourceful/resourcefullib/common/item/tabs/fabric/ResourcefulCreativeTabImpl.java
+++ fabric/src/main/java/com/teamresourceful/resourcefullib/common/item/tabs/fabric/ResourcefulCreativeTabImpl.java
@@ -2,6 +2,8 @@ package com.teamresourceful.resourcefullib.common.item.tabs.fabric;
 
 import com.teamresourceful.resourcefullib.common.item.tabs.ResourcefulCreativeTab;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
 
@@ -9,7 +11,7 @@ import java.util.function.Supplier;
 
 public class ResourcefulCreativeTabImpl {
     public static Supplier<CreativeModeTab> create(ResourcefulCreativeTab tab) {
-        var group = FabricItemGroup.builder(tab.id)
+        var group = FabricItemGroup.builder()
                 .icon(() -> tab.icon.get())
                 .title(Component.translatable("itemGroup." + tab.id.getNamespace() + "." + tab.id.getPath()));
         if (tab.hideScrollBar) group.noScrollBar();
@@ -19,6 +21,7 @@ public class ResourcefulCreativeTabImpl {
             tab.stacks.stream().map(Supplier::get).forEach(output::accept);
         });
         CreativeModeTab tab1 = group.build();
+        Registry.register(BuiltInRegistries.CREATIVE_MODE_TAB, tab.id, tab1);
         return () -> tab1;
     }
 }
