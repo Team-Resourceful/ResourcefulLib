bf1904a8c5b68f992471b49e9d8c730d40341ae4
ThatGravyBoat <thatgravyboat@gmail.com>
2023-07-11 21:28:36 -0230

    Register ingredients properly on forge
    <log>Register ingredients properly on forge.</log>

--- forge/src/main/java/com/teamresourceful/resourcefullib/common/recipe/ingredient/forge/ForgeIngredientHelper.java
+++ forge/src/main/java/com/teamresourceful/resourcefullib/common/recipe/ingredient/forge/ForgeIngredientHelper.java
@@ -3,6 +3,7 @@ package com.teamresourceful.resourcefullib.common.recipe.ingredient.forge;
 import com.teamresourceful.resourcefullib.common.recipe.ingredient.CodecIngredient;
 import com.teamresourceful.resourcefullib.common.recipe.ingredient.CodecIngredientSerializer;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.common.crafting.CraftingHelper;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -16,6 +17,8 @@ public final class ForgeIngredientHelper {
     }
 
     public static <T extends CodecIngredient<T>> void register(CodecIngredientSerializer<T> serializer) {
-        SERIALIZERS.put(serializer.id(), new ForgeIngredientSerializer<>(serializer));
+        ForgeIngredientSerializer<T> forgeSerializer = new ForgeIngredientSerializer<>(serializer);
+        SERIALIZERS.put(serializer.id(), forgeSerializer);
+        CraftingHelper.register(serializer.id(), forgeSerializer);
     }
 }
