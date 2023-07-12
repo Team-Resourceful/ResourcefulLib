5ba6b467aad3a44e2f81c3ddc94bd230a927a84a
ThatGravyBoat <thatgravyboat@gmail.com>
2023-07-11 21:30:06 -0230

    Add map tag and map collection methods for tags.
    <log>Add map tag and map collection methods for tags.</log>

--- common/src/main/java/com/teamresourceful/resourcefullib/common/nbt/TagUtils.java
+++ common/src/main/java/com/teamresourceful/resourcefullib/common/nbt/TagUtils.java
@@ -6,7 +6,13 @@ import net.minecraft.nbt.ListTag;
 import net.minecraft.nbt.Tag;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 public final class TagUtils {
 
@@ -30,5 +36,27 @@ public final class TagUtils {
         return list.stream().filter(tagClass::isInstance).map(tagClass::cast).toList();
     }
 
+    public static <K, V> Map<K, V> mapTag(CompoundTag tag, Function<String, K> keyMapper, BiFunction<String, CompoundTag, V> valueMapper) {
+        Map<K, V> map = new HashMap<>();
+        for (String key : tag.getAllKeys()) {
+            map.put(keyMapper.apply(key), valueMapper.apply(key, tag));
+        }
+        return map;
+    }
 
+    public static <C extends Collection<T>, T> C mapToCollection(Supplier<C> collection, ListTag list, Function<Tag, T> mapper) {
+        C c = collection.get();
+        for (Tag tag : list) {
+            c.add(mapper.apply(tag));
+        }
+        return c;
+    }
+
+    public static <T> ListTag mapToListTag(Collection<T> collection, Function<T, Tag> mapper) {
+        ListTag list = new ListTag();
+        for (T t : collection) {
+            list.add(mapper.apply(t));
+        }
+        return list;
+    }
 }
