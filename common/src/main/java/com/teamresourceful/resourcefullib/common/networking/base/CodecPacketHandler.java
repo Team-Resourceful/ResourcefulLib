3a0c0a0aa39f2f8c65ecd83f366fd44f7d7fb667
ThatGravyBoat <thatgravyboat@gmail.com>
2023-07-28 07:17:59 -0230

    Add ByteCodec packets
    <log>Added new experimental ByteCodec packets</log>

---
+++ common/src/main/java/com/teamresourceful/resourcefullib/common/networking/base/CodecPacketHandler.java
@@ -0,0 +1,32 @@
+package com.teamresourceful.resourcefullib.common.networking.base;
+
+import com.teamresourceful.bytecodecs.base.ByteCodec;
+import net.minecraft.network.FriendlyByteBuf;
+import org.jetbrains.annotations.ApiStatus;
+
+/**
+ * This class is a {@link PacketHandler} that uses a {@link ByteCodec} to encode and decode packets.
+ * <br>
+ * @apiNote This class is experimental as the stability of the {@link ByteCodec} API is not guaranteed.
+ *
+ * @param <T> The type of packet this handler handles.
+ */
+@ApiStatus.Experimental
+public abstract class CodecPacketHandler<T extends Packet<T>> implements PacketHandler<T> {
+
+    protected final ByteCodec<T> codec;
+
+    public CodecPacketHandler(ByteCodec<T> codec) {
+        this.codec = codec;
+    }
+
+    @Override
+    public void encode(T message, FriendlyByteBuf buffer) {
+        codec.encode(message, buffer);
+    }
+
+    @Override
+    public T decode(FriendlyByteBuf buffer) {
+        return codec.decode(buffer);
+    }
+}
