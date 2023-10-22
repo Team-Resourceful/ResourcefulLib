f65be35c78ff6e6b9e5648f93783361236d52475
ThatGravyBoat <thatgravyboat@gmail.com>
2023-10-22 12:37:29 -0230

    Add strict mode for nbt predicate
    <log>Added strict mode for nbt predicates</log>

--- common/src/main/java/com/teamresourceful/resourcefullib/common/codecs/predicates/NbtPredicate.java
+++ common/src/main/java/com/teamresourceful/resourcefullib/common/codecs/predicates/NbtPredicate.java
@@ -1,14 +1,13 @@
 package com.teamresourceful.resourcefullib.common.codecs.predicates;
 
 import com.mojang.serialization.Codec;
-import net.minecraft.nbt.CompoundTag;
-import net.minecraft.nbt.ListTag;
-import net.minecraft.nbt.NbtUtils;
-import net.minecraft.nbt.Tag;
+import net.minecraft.nbt.*;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import org.jetbrains.annotations.Nullable;
+
+import java.math.BigDecimal;
 
 public record NbtPredicate(CompoundTag tag) {
 
@@ -26,8 +25,60 @@ public record NbtPredicate(CompoundTag tag) {
     }
 
     public boolean matches(@Nullable Tag tag) {
-        if (this == ANY || isEmpty(this.tag)) return true;
-        return isEmpty(tag) ? isEmpty(this.tag) : NbtUtils.compareNbt(this.tag, tag, true);
+        return this.matches(tag, true);
+    }
+
+    public boolean matches(@Nullable Tag tag, boolean strict) {
+        if (this == ANY) return true;
+        boolean empty = isEmpty(this.tag);
+        if (isEmpty(tag)) return empty;
+        return strict ? NbtUtils.compareNbt(this.tag, tag, true) : compareNbt(this.tag, tag);
+    }
+
+    public static boolean compareNbt(@Nullable Tag tag, @Nullable Tag tag2) {
+        if (tag == tag2) {
+            return true;
+        } else if (tag == null || tag2 == null) {
+            return true;
+        } else if (tag instanceof NumericTag num1 && tag2 instanceof NumericTag num2) {
+            BigDecimal bigDecimal = new BigDecimal(num1.toString());
+            BigDecimal bigDecimal2 = new BigDecimal(num2.toString());
+            return bigDecimal.compareTo(bigDecimal2) == 0;
+        } else if (!tag.getClass().equals(tag2.getClass())) {
+            return false;
+        } else if (tag instanceof CompoundTag compound) {
+            CompoundTag compound2 = (CompoundTag) tag2;
+            for (String key : compound.getAllKeys()) {
+                if (!compareNbt(compound.get(key), compound2.get(key))) {
+                    return false;
+                }
+            }
+            return true;
+        } else if (tag instanceof ListTag list) {
+            ListTag list2 = (ListTag) tag2;
+            if (list.isEmpty()) {
+                return list2.isEmpty();
+            } else {
+                for (Tag entry : list) {
+                    boolean oneMatches = false;
+
+                    for (Tag value : list2) {
+                        if (compareNbt(entry, value)) {
+                            oneMatches = true;
+                            break;
+                        }
+                    }
+
+                    if (!oneMatches) {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+        } else {
+            return tag.equals(tag2);
+        }
     }
 
     public static boolean isEmpty(Tag tag) {
