bf68969ea945e046288541c69d6f9e54fe18995e
ThatGravyBoat <thatgravyboat@gmail.com>
2023-11-26 18:19:24 -0330

    Remove nonextendable annotations
    <log>Removed NonExtendable annotations</log>

--- common/src/main/java/com/teamresourceful/resourcefullib/common/network/base/ClientboundPacketType.java
+++ common/src/main/java/com/teamresourceful/resourcefullib/common/network/base/ClientboundPacketType.java
@@ -1,9 +1,7 @@
 package com.teamresourceful.resourcefullib.common.network.base;
 
 import com.teamresourceful.resourcefullib.common.network.Packet;
-import org.jetbrains.annotations.ApiStatus;
 
-@ApiStatus.NonExtendable
 public interface ClientboundPacketType<T extends Packet<T>> extends PacketType<T> {
 
     Runnable handle(T message);
