8dc854d525fd33a4fa0fdb0fddd9b6ca64f56917
ThatGravyBoat <thatgravyboat@gmail.com>
2024-05-12 12:33:34 -0230

    Mark fluids as experimental

---
+++ common/src/main/java/com/teamresourceful/resourcefullib/common/fluid/package-info.java
@@ -0,0 +1,8 @@
+/*
+ * This package is experimental because it has not been tested enough to be considered stable.
+ */
+
+@ApiStatus.Experimental
+package com.teamresourceful.resourcefullib.common.fluid;
+
+import org.jetbrains.annotations.ApiStatus;
--- common/src/main/java/com/teamresourceful/resourcefullib/common/registry/ResourcefulRegistryType.java
+++ common/src/main/java/com/teamresourceful/resourcefullib/common/registry/ResourcefulRegistryType.java
@@ -2,9 +2,11 @@ package com.teamresourceful.resourcefullib.common.registry;
 
 import com.teamresourceful.resourcefullib.common.fluid.data.FluidData;
 import com.teamresourceful.resourcefullib.common.fluid.registry.ResourcefulFluidRegistry;
+import org.jetbrains.annotations.ApiStatus;
 
 public final class ResourcefulRegistryType<D, T extends ResourcefulRegistry<D>> {
 
+    @ApiStatus.Experimental
     public static final ResourcefulRegistryType<FluidData, ResourcefulFluidRegistry> FLUID = new ResourcefulRegistryType<>(
             ResourcefulFluidRegistry.class
     );
