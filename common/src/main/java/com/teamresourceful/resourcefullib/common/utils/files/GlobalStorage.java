cf04d3927775e2c7b00a30634d9fc2322ca9fef3
ThatGravyBoat <thatgravyboat@gmail.com>
2024-06-23 11:20:06 -0230

    Add global storage

---
+++ common/src/main/java/com/teamresourceful/resourcefullib/common/utils/files/GlobalStorage.java
@@ -0,0 +1,93 @@
+package com.teamresourceful.resourcefullib.common.utils.files;
+
+import com.teamresourceful.resourcefullib.common.lib.Constants;
+import net.minecraft.Optionull;
+import org.jetbrains.annotations.ApiStatus;
+import org.lwjgl.system.Platform;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * Creates and manages the cache and data directories for mods.
+ * <p>
+ * You may use the getCacheDirectory and getDataDirectory methods to get the directories for your mod.
+ * <p>
+ * This does not handle any of the file IO for you, it only provides the directories paths.
+ * You must handle the file IO and creation of the directories yourself.
+ */
+public class GlobalStorage {
+
+    private static final String ID = "resourcefullib";
+    private static final String README = """
+            This is created by ResourcefulLib for usage by mods.
+            This directory is used to store data/caches for mods.
+            """;
+
+    private static final Path cache;
+    private static final Path data;
+
+    static {
+        switch (Platform.get()) {
+            case WINDOWS -> {
+                cache = Paths.get(System.getenv("LOCALAPPDATA"), "." + ID, "cache");
+                data = Paths.get(System.getenv("LOCALAPPDATA"), "." + ID, "data");
+            }
+            case MACOSX -> {
+                cache = Paths.get(System.getProperty("user.home"), "Library", "Caches", ID);
+                data = Paths.get(System.getProperty("user.home"), "Library", "Application Support", ID);
+            }
+            default -> {
+                cache = Optionull.mapOrElse(
+                        System.getenv("XDG_CACHE_HOME"),
+                        home -> Path.of(home, ID),
+                        () -> Path.of(System.getProperty("user.home"), ".cache", ID)
+                );
+                data = Optionull.mapOrElse(
+                        System.getenv("XDG_DATA_HOME"),
+                        home -> Path.of(home, ID),
+                        () -> Path.of(System.getProperty("user.home"), ".local", "share", ID)
+                );
+            }
+        }
+    }
+
+    @ApiStatus.Internal
+    public static void init() {
+        try {
+            cache.toFile().mkdirs();
+            data.toFile().mkdirs();
+
+            Path readme = cache.resolve("README.txt");
+            if (!Files.exists(readme)) {
+                Files.writeString(readme, README);
+            }
+
+            readme = data.resolve("README.txt");
+            if (!Files.exists(readme)) {
+                Files.writeString(readme, README);
+            }
+        } catch (Exception e) {
+            Constants.LOGGER.warn("Failed to create cache/data directories!", e);
+        }
+    }
+
+    /**
+     * Returns the cache directory for the mod.
+     * @param modid The modid of the mod.
+     * @return The cache directory for the mod.
+     */
+    public static Path getCacheDirectory(String modid) {
+        return cache.resolve(modid);
+    }
+
+    /**
+     * Returns the data directory for the mod.
+     * @param modid The modid of the mod.
+     * @return The data directory for the mod.
+     */
+    public static Path getDataDirectory(String modid) {
+        return data.resolve(modid);
+    }
+}
