0ce13da6a73f782b27e3e31f90044552b5039ef6
Gravy Boat <gravy@thatgravyboat.tech>
2026-03-24 16:35:31 -0230

    chore: update to 26.1
    <log>Update to 26.1</log>

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,10 +36,6 @@ subprojects {
         }
     }
 
-    repositories {
-        maven("https://prmaven.neoforged.net/NeoForge/pr2879")
-    }
-
     dependencies {
         if (platform == Platform.COMMON) {
             "api"(rootProject.libs.yabn)
