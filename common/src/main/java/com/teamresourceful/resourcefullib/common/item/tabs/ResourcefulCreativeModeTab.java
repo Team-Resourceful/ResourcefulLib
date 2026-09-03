16bf485c35484a86a31c901ccea5197612c124f8
epicoreo <th3darkrose@gmail.com>
2026-09-03 01:15:13 -0400

    add convenience method to ResourcefulCreativeModeTab
    <log>added convenience method to ResourcefulCreativeModeTab for sorting multiple registries</log>

--- common/src/main/java/com/teamresourceful/resourcefullib/common/item/tabs/ResourcefulCreativeModeTab.java
+++ common/src/main/java/com/teamresourceful/resourcefullib/common/item/tabs/ResourcefulCreativeModeTab.java
@@ -1,12 +1,14 @@
 package com.teamresourceful.resourcefullib.common.item.tabs;
 
 import com.teamresourceful.resourcefullib.common.registry.ResourcefulRegistry;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.Identifier;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.ItemLike;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.function.Supplier;
 import java.util.stream.Stream;
@@ -47,6 +49,20 @@ public class ResourcefulCreativeModeTab {
 
     public <I extends ItemLike, T extends ResourcefulRegistry<I>> ResourcefulCreativeModeTab addRegistry(T registry) {
         return addContent(() -> registry.boundStream().map(ItemStack::new));
+    }
+
+    @SafeVarargs
+    public final <I extends ItemLike> ResourcefulCreativeModeTab addAndSortRegistries(ResourcefulRegistry<? extends I>... registries) {
+        return addContent(() ->
+                Stream.of(registries)
+                        .flatMap(ResourcefulRegistry::boundStream)
+                        .sorted(Comparator.comparing(item ->
+                                BuiltInRegistries.ITEM
+                                        .getKey(item.asItem())
+                                        .toString()
+                        ))
+                        .map(ItemStack::new)
+        );
     }
 
     public ResourcefulCreativeModeTab addStack(Supplier<ItemStack> stack) {
